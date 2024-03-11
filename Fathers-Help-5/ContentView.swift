fe1486795a53350fa87999c24faec719dfe756fa
umpu <carol.ledgers0h@icloud.com>
2024-03-11 19:50:10 +0500

    Initial Commit

---
+++ Fathers-Help-5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fathers-Help-5
+//
+//  Created by Andrei Gorbunov on 11.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fathers-Help-5/Fathers_Help_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Fathers_Help_5App.swift
+//  Fathers-Help-5
+//
+//  Created by Andrei Gorbunov on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Fathers_Help_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
